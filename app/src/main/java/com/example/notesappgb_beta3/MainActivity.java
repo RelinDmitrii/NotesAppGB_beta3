eed0b0f40e7be69f6bd6dab6e3e11cf315f7e533
RelinDmitrii <relin-dmitrii@yandex.ru>
2021-02-18 12:34:35 +0300

    MainActivity

--- app/src/main/java/com/example/notesappgb_beta3/MainActivity.java
+++ app/src/main/java/com/example/notesappgb_beta3/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.notesappgb_beta3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+            Intent intent = getIntent();
+            int index = intent.getIntExtra(NotesFragment.ARG_INDEX, 0);
+            NotesFragment notesFragment = NotesFragment.newInstance(index);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.layout_container, notesFragment);
+            fragmentTransaction.commit();
+        }
+
     }
 }
